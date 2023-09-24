9dae15bcb6f48f43147c0a7f1b8e68f7e4397136
kin tirumala <25tirumala@stfrancishs.org>
2023-09-23 20:00:02 -0700

    Co-authored-by: akrenomeron <akrenomeron@users.noreply.github.com> added a pink button to the gui

--- gui2324.py
+++ gui2324.py
@@ -1,2 +1,44 @@
-import socket
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
+from PyQt5.QtGui import QIcon, QFont, QColor
 
+
+
+class Example(QWidget):
+    
+
+    def __init__(self): #initialize the sle 
+        super().__init__() #returns the parent object of the Example class
+
+        self.initUI() #create the gui
+
+
+    def initUI(self):
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('this is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('this is a <b><font color = pink>bye</font></b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+        btn.setStyleSheet("background-color: pink;"
+                          "font color: white;")
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
