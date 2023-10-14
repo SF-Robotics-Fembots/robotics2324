b63568f1c19b9a179e9434cb07139a0cdaaeaafa
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2023-10-14 10:36:54 -0700

    gui2324.py

--- gui2324.py
+++ gui2324.py
@@ -1,42 +1,141 @@
+import socket
+import time
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
-from PyQt5.QtGui import QIcon, QFont, QColor
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
 
-class Example(QWidget):
+class Window(QMainWindow):
     
-
-    def __init__(self): #initialize the sle 
+    '''
+    def __init__(self): #initialize the self (sle?) 
         super().__init__() #returns the parent object of the Example class
 
-        self.initUI() #create the gui
-
+        self.initUI() #create the gui '''
 
     def initUI(self):
+        super().__init__()
+        self.initUI()
+        self.setWindowTitle('GUI')
+        self.setWindowIcon(QIcon('web.png'))
+        self.setGeometry(200, 120, 2500, 1500) #(x-coor, y-coor, width, height)
+        self.UiComponents()
+        self.show()
 
-        QToolTip.setFont(QFont('SansSerif', 10))
 
-        self.setToolTip('this is a <b>QWidget</b> widget')
-
+        '''button styling
         btn = QPushButton('Button', self)
         btn.setToolTip('this is a <b><font color = pink>bye</font></b> widget')
         btn.resize(btn.sizeHint())
         btn.move(50, 50)
         btn.setStyleSheet("background-color: pink;"
-                          "font color: white;")
+                          "font color: white;")'''
 
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('web.png'))
+    def UiComponents(self):
+        
+        #count variable
+        '''self.start = 15
+        self.time = self.start * 60
+        self.countdownsec = True
+        self.count = 0'''
 
-        self.show()
+        #start switch
+        #self.start = False
 
+        # creating label to show the seconds
+        self.label = QLabel("15 : 00", self)
+ 
+        # setting geometry of label
+        self.label.setGeometry(100, 200, 200, 50)
+ 
+        # setting border to the label
+        self.label.setStyleSheet("border : 3px solid black")
+ 
+        # setting font to the label
+        self.label.setFont(QFont('Times', 15))
+ 
+        # setting alignment to the label
+        self.label.setAlignment(Qt.AlignCenter)
+        
+        # creating start button
+        start_button = QPushButton("Start", self)
+ 
+        # setting geometry to the button
+        start_button.setGeometry(125, 350, 150, 40)
+ 
+        # creating pause button
+        pause_button = QPushButton("Pause", self)
+ 
+        # setting geometry to the button
+        pause_button.setGeometry(125, 400, 150, 40)
+ 
+        # creating reset  button
+        reset_button = QPushButton("Reset", self)
+ 
+        # setting geometry to the button
+        reset_button.setGeometry(125, 450, 150, 40)
+ 
+        # creating a timer object
+        timer = QTimer(self)
+
+        #timer.timeout.connect(self.updateTime)
+
+        #timer.start(100)
+
+    # method called by timer
+    '''def updateTime(self):
+        if self.start:
+                # incrementing the counter
+            self.minutes = self.time / 60
+            self.seconds = self.time % 60
+
+    
+                # timer is completed
+            if self.count == 0:
+    
+                # making flag false
+                self.start = False
+
+                    # getting text from count
+        if self.start:
+            text = str(self.minutes) + " : " + str(self.seconds)
+ 
+            # showing text
+            self.label.setText(text)
+
+    def start_action(self):
+            # making flag true
+            self.start = True
+    
+            # count = 0
+            if self.count == 0:
+                self.start = False
+    
+    def pause_action(self):
+    
+        # making flag false
+        self.start = False
+    
+    def reset_action(self):
+    
+        # making flag false
+        self.start = False
+    
+        # setting count value to 0
+        self.count = 0
+'''
 
 def main():
 
     app = QApplication(sys.argv)
-    ex = Example()
+    mainwindow = QMainWindow()
+    window = Window()
+    window.setupUi(mainwindow)
+    mainwindow.show()
+    
     sys.exit(app.exec_())
 
 
