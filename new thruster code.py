f5ed55d51c75d28c78dfd8e30ec380f79a20d66b
kin tirumala <25Rosario@stfrancishs.org>
2023-11-04 13:34:42 -0700

    Update new thruster code.py

--- new thruster code.py
+++ new thruster code.py
@@ -19,7 +19,6 @@ def main():
 	shield.frequency = 100
 
 	thrusterChannel = array[shield.channels[0], shield.channels[1], shield.channels[2], shield.channels[3], shield.channels[4], shield.channels[5], shield.channels[6]]
-	
 	throttle_in = 1480
 	throttlePW = int(throttle_in/10000*65536)
 	thrusterChannel.duty_cycle = throttlePW
