1f8cb0f349585247ab6ed217eb7f6b8ee2aa7bf7
kin tirumala <25tirumala@stfrancishs.org>
2023-12-11 17:51:09 -0800

    more code for the gripper stuff

--- gripperbottomside.py
+++ gripperbottomside.py
@@ -1,22 +1,35 @@
-import time, RPi.GPIO as GPIO, socket
+#import time, RPi.GPIO as GPIO
+import socket, time
 
 #setup for the board and the pins
-GPIO.setmode(GPIO.BCM) #set this to the gpio pins
+#GPIO.setmode(GPIO.BCM) #set this to the gpio pins
 #both GPIO 23 and 24 for the outputs
-GPIO.setup(23, GPIO.OUT) #actually pin 16
-GPIO.setup(24, GPIO.OUT) #actually pin 18
+#GPIO.setup(23, GPIO.OUT) #actually pin 16
+#GPIO.setup(24, GPIO.OUT) #actually pin 18
 
 #set up the socket connection ugh
 #bottomside is client
 #global server_ip, port will be defined in the threading program
 #server_ip = "192.168.1.100"
 port = 40000 #is up for debate
+ip_server = "127.0.0.1"
 
 
-def main(server_ip):
-    clientsocket = socket(socket.SOCKSTREAM, socket.AF_INET)
-    clientsocket.connect((server_ip, port))
-    gripper_on = data
+def main():
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((ip_server, port))
+    while True: 
+        print("client connected!")
+        data = clientsocket.recv(1024)
+        data = data.decode()
+        print(data)
+
+        time.sleep(1.5)
+        break
+    print("socket closing!")
+    clientsocket.close()
+        
+    
 
 
 
--- grippertopside.py
+++ grippertopside.py
@@ -32,61 +32,70 @@ mcp.configure_spi_timing(chip_select_to_data_delay=0,
 
 #port for the socket
 port = 40000
+ip_server = "127.0.0.1"
 
-
-def main(ip_server):
+def main():
 
     # set all pins as GPIO
     for i in range(9):
         mcp.set_gpio_designation(i, Mcp2210GpioDesignation.GPIO)
 
-    # set lower GPIOs to output
-    for i in range(0, 5):
-        mcp.set_gpio_direction(i, Mcp2210GpioDirection.OUTPUT)
-
-    # set upper GPIOs to input
-    for i in range(5, 9):
-        mcp.set_gpio_direction(i, Mcp2210GpioDirection.INPUT)
-        print("Pin {}:".format(i), mcp.get_gpio_value(i))
+    # set 2 to input
+    mcp.set_gpio_direction(2, Mcp2210GpioDirection.INPUT)
+    #the while 1 acts as a while true
+    while 1:
+        x = mcp.get_gpio_value(2)
+        print(x)
 
     #set up a server socket
-    serversocket = socket.socket(socket.SOCKSTREAM, socket.AF_INET)
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((ip_server, port))
     serversocket.listen(1)
     print("socket listening")
     
-    (clientConnected, clientAddress) = serversocket.accept()
-
+    (clientconnected, clientaddress) = serversocket.accept()
     while True:
+
+
+        
+        message = "hi"
+
+        data  = message.encode()
+        clientconnected.send(data)
+
+        serversocket.close()
+
+
+
         # flash an LED
         # set_gpio_output_value(<pin_number>,<on or off>)
-        mcp.set_gpio_output_value(0, False)
-        for i in range(3):
-            mcp.set_gpio_output_value(0, True)
-            time.sleep(0.5)
-            mcp.set_gpio_output_value(0, False)
-            time.sleep(0.5)
+        #mcp.set_gpio_output_value(0, False)
+        #for i in range(3):
+           # mcp.set_gpio_output_value(0, True)
+           # time.sleep(0.5)
+           # mcp.set_gpio_output_value(0, False)
+           # time.sleep(0.5)
 
         # LED slider
-        counter = 0
-        for _ in range(20):
-            counter += 1
-            counter %= 5
-            for i in range(5):
-                mcp.set_gpio_output_value(i, counter == i)
-            time.sleep(0.2)
+        #counter = 0
+        #for _ in range(20):
+         #   counter += 1
+          #  counter %= 5
+           # for i in range(5):
+            #    mcp.set_gpio_output_value(i, counter == i)
+            #time.sleep(0.2)
 
         # turn all LEDs off
-        for i in range(0, 5):
-            mcp.set_gpio_output_value(i, False)
+        #for i in range(0, 5):
+         #   mcp.set_gpio_output_value(i, False)
 
         # set pin 4 as CS, and transmit the bytes 0 through to 255 inclusive over SPI
-        mcp.set_gpio_designation(4, Mcp2210GpioDesignation.CHIP_SELECT)
-        tx_data = bytes(range(256))
-        rx_data = mcp.spi_exchange(tx_data, cs_pin_number=4)
+        #mcp.set_gpio_designation(4, Mcp2210GpioDesignation.CHIP_SELECT)
+        #tx_data = bytes(range(256))
+        #rx_data = mcp.spi_exchange(tx_data, cs_pin_number=4)
 
         # as MOSI is connected to MISO, check that the data matches what we sent
-        assert rx_data == tx_data
+        #assert rx_data == tx_data
 
     
 
