085c976201c54c4582fc0cf081ad0921e7a1110e
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2024-01-06 13:21:50 -0800

    save

--- cameragui2324.py
+++ cameragui2324.py
@@ -1,115 +1,179 @@
-
-import sys
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 import cv2
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
+    QLabel, QGridLayout, QScrollArea, QSizePolicy
+from PyQt5.QtGui import QPixmap, QIcon, QImage, QPalette
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QEvent, QObject
+from PyQt5 import QtCore
+import sys
 
-#create a holder for the url
+class CaptureCam(QThread):
+    ImageUpdate = pyqtSignal(QImage)
 
+    def __init__(self, camera_id):
+        super(CaptureCam, self).__init__()
+        self.ThreadActive = True
+        self.camera_id = 0
+        if self.camera_id == 0:
+            self.camera = 0
+        elif self.camera_id == 1:
+            self.camera = "http://192.168.1.99:8080/stream"
+        
+
+    def run(self) -> None:
+        self.ThreadActive = True
+        capture = cv2.VideoCapture(self.camera_id)
+        while self.ThreadActive:
+            ret, frame = capture.read()
+            if ret:
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImage = cv2.flip(Image, 1) #mirrors
+                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1],
+                                           FlippedImage.shape[0], QImage.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImageUpdate.emit(Pic)
+
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        
+        self.camera_1 = 0
+        self.camera_2 = 1
+        self.camera_3 = 2
+        self.camera_4 = 3
+        self.camera_5 = 4
 
-        self.setStyleSheet("background-color: grey;")
-        self.setGeometry(400, 270, 1900, 900)
+        self.camera_1 = QLabel()
+        self.camera_1.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.camera_1.setScaledContents(True)
+        self.camera_1.installEventFilter(self)
+        self.camera_1.setObjectName("Camera_1")
 
-        self.widget = QWidget()
-        VBL = QVBoxLayout()
-        self.widget.setLayout(VBL)
+        self.QScrollArea_1 = QScrollArea()
+        self.QScrollArea_1.setBackgroundRole(QPalette.Dark)
+        self.QScrollArea_1.setWidgetResizable(True)
+        self.QScrollArea_1.setWidget(self.camera_1)
+
+        self.camera_2 = QLabel()
+        self.camera_2.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.camera_2.setScaledContents(True)
+        self.camera_2.installEventFilter(self)
+        self.camera_2.setObjectName("Camera_2")
+
+        self.QScrollArea_2 = QScrollArea()
+        self.QScrollArea_2.setBackgroundRole(QPalette.Dark)
+        self.QScrollArea_2.setWidgetResizable(True)
+        self.QScrollArea_2.setWidget(self.camera_2)
+
+        self.camera_3 = QLabel()
+        self.camera_3.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.camera_3.setScaledContents(True)
+        self.camera_3.installEventFilter(self)
+        self.camera_3.setObjectName("Camera_3")
+
+        self.QScrollArea_3 = QScrollArea()
+        self.QScrollArea_3.setBackgroundRole(QPalette.Dark)
+        self.QScrollArea_3.setWidgetResizable(True)
+        self.QScrollArea_3.setWidget(self.camera_3)
+
+        self.camera_4 = QLabel()
+        self.camera_4.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.camera_4.setScaledContents(True)
+        self.camera_4.installEventFilter(self)
+        self.camera_4.setObjectName("Camera_4")
+
+        self.QScrollArea_4 = QScrollArea()
+        self.QScrollArea_4.setBackgroundRole(QPalette.Dark)
+        self.QScrollArea_4.setWidgetResizable(True)
+        self.QScrollArea_4.setWidget(self.camera_4)
+
+        self.camera_5 = QLabel()
+        self.camera_5.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.camera_5.setScaledContents(True)
+        self.camera_5.installEventFilter(self)
+        self.camera_5.setObjectName("Camera_5")
+
+        self.QScrollArea_5 = QScrollArea()
+        self.QScrollArea_5.setBackgroundRole(QPalette.Dark)
+        self.QScrollArea_5.setWidgetResizable(True)
+        self.QScrollArea_5.setWidget(self.camera_5)
+
+        self.SetupUI()
+
+        self.CaptureCam_1 = CaptureCam(self.camera_1)
+        self.CaptureCam_1.ImageUpdate.connect(lambda image: self.ShowCamera1(image))
+
+        self.CaptureCam_2 = CaptureCam(self.camera_2)
+        self.CaptureCam_2.ImageUpdate.connect(lambda image: self.ShowCamera2(image))
+
+        self.CaptureCam_3 = CaptureCam(self.camera_3)
+        self.CaptureCam_3.ImageUpdate.connect(lambda image: self.ShowCamera3(image))
+
+        self.CaptureCam_4 = CaptureCam(self.camera_4)
+        self.CaptureCam_4.ImageUpdate.connect(lambda image: self.ShowCamera4(image))
+
+        self.CaptureCam_5 = CaptureCam(self.camera_2)
+        self.CaptureCam_5.ImageUpdate.connect(lambda image: self.ShowCamera5(image))
+
+        self.CaptureCam_1.start()
+        self.CaptureCam_2.start()
+        self.CaptureCam_3.start()
+        self.CaptureCam_4.start()
+        self.CaptureCam_5.start()
+    
+    def SetupUI(self):
+        grid_layout = QGridLayout()
+        grid_layout.setContentsMargins(0, 0, 0, 0)
+        grid_layout.addWidget(self.QScrollArea_1, 0, 0)
+        grid_layout.addWidget(self.QScrollArea_2, 0, 1)
+        grid_layout.addWidget(self.QScrollArea_3, 0, 2)
+        grid_layout.addWidget(self.QScrollArea_4, 1, 0)
+        grid_layout.addWidget(self.QScrollArea_5, 1, 2)
+
+        self.widget = QWidget(self)
+        self.widget.setLayout(grid_layout)
+
         self.setCentralWidget(self.widget)
+        self.setMinimumSize(800, 600)
+        self.showMaximized()
+        self.setStyleSheet("QMainWindow {background: 'black';}")
+        self.setWindowTitle("Camera GUI")
 
-        self.feed = QLabel(self)
-        self.feed.setToolTip("feedlabel")
+    @QtCore.pyqtSlot()
+    def ShowCamera1(self, frame: QImage) -> None:
+        self.camera_1.setPixmap(QPixmap.fromImage(frame))
 
-        self.camera_label = QLabel("CAMERAS", self)
-        self.camera_label.setGeometry(75, 375, 450, 120)
-        self.camera_label.setStyleSheet("border: 4px solid white; border-radius: 10; background: #5D7090; color: #F1F6FD")
-        self.camera_label.setFont(QFont('Time', 15))
-       # self.camera_label.setAlignment(Qt.AlignCenter)
-       # self.camera_label.move(1380, 90)
-        #self.VBL.addWidget(self.feed)
-
-        #VBL.addWidget(self.Worker)
-        #VBL.addWidget(self.Worker1)
-        VBL.addWidget(self.camera_label)
-        VBL.addWidget(self.CancelBTN)
-
-        #self.FeedLabel2 = QLabel()
-        #self.FeedLabel2.setToolTip("feedlabel2")
-       # self.VBL.addWidget(self.FeedLabel2)
+    @QtCore.pyqtSlot()
+    def ShowCamera2(self, frame: QImage) -> None:
+        self.camera_2.setPixmap(QPixmap.fromImage(frame))    
         
+    @QtCore.pyqtSlot()
+    def ShowCamera3(self, frame: QImage) -> None:
+        self.camera_3.setPixmap(QPixmap.fromImage(frame)) 
 
-        self.CancelBTN = QPushButton("cancel")
-        self.CancelBTN.clicked.connect(self.CancelFeed)
-        self.VBL.addWidget(self.CancelBTN)
+    @QtCore.pyqtSlot()
+    def ShowCamera4(self, frame: QImage) -> None:
+        self.camera_4.setPixmap(QPixmap.fromImage(frame)) 
 
-        self.Worker = Worker() #create a worker cam object
-        self.Worker1 = Worker1()
-        #self.newLabel = QLabel(self.Worker1)
-        #self.newLabel.move(1400, 75)
-        #self.Worker1.move(1400, 220) 
+    @QtCore.pyqtSlot()
+    def ShowCamera5(self, frame: QImage) -> None:
+        self.camera_5.setPixmap(QPixmap.fromImage(frame)) 
 
-        self.Worker.start()
-        self.Worker.ImageUpdate.connect(self.ImageUpdateSlot)
-        
-
-        self.Worker1.start()
-        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
-        self.setLayout(self.VBL)
-
-    
-    def ImageUpdateSlot(self, Image):
-        self.feed.setPixmap(QPixmap.fromImage(Image))
-        
-
-    def CancelFeed(self):
-        self.Worker.stop()
-        self.Worker1.stop()
-
-class Worker(QThread):
-    ImageUpdate = pyqtSignal(QImage)
-    def run(self):
-        self.ThreadActive = True
-        capture = cv2.VideoCapture("http://192.168.1.99:8080/stream")
-        while self.ThreadActive:
-            ret, frame = capture.read()
-            if ret:
-                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                FlippedImage = cv2.flip(Image, 1) #mirrors
-                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1],
-                                           FlippedImage.shape[0], QImage.Format_RGB888)
-                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                self.ImageUpdate.emit(Pic)
-                #capture.release()
-    def stop(self):
-        self.ThreadActive = False
-        self.quit()
-
-class Worker1(QThread):
-    ImageUpdate = pyqtSignal(QImage)
-    def run(self):
-        self.ThreadActive = True
-        capture = cv2.VideoCapture(0)
-        while self.ThreadActive:
-            ret, frame = capture.read()
-            if ret:
-                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                FlippedImage = cv2.flip(Image, 1) #mirrors
-                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1],
-                                           FlippedImage.shape[0], QImage.Format_RGB888)
-                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                self.ImageUpdate.emit(Pic)
-                #capture.release()
-    def stop(self):
-        self.ThreadActive = False
-        self.quit()
-
-    
-
-if __name__ == "__main__":
+def main() -> None:
+    # Create a QApplication object. It manages the GUI application's control flow and main settings.
+    # It handles widget specific initialization, finalization.
+    # For any GUI application using Qt, there is precisely one QApplication object
     app = QApplication(sys.argv)
-    root = MainWindow()
-    root.show()
-    sys.exit(app.exec())
+    # Create an instance of the class MainWindow.
+    window = MainWindow()
+    # Show the window.
+    window.show()
+    # Start Qt event loop.
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
