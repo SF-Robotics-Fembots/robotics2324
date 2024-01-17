b95277269f7877224fe9fc4d5c35f1e31611ebf5
kin tirumala <25tirumala@stfrancishs.org>
2024-01-16 17:27:23 -0800

    changing the thruster numbers

--- mainServer.py
+++ mainServer.py
@@ -3,7 +3,7 @@ import time
 import socket
 import threading
 import joystickServer
-import grippertopside
+#import grippertopside
 #import lightServer
 #import hatTest
 #import saltWater
@@ -12,19 +12,20 @@ import pilotInverseServer
 #import fryServer
 
 global ip_server
-ip_server = "192.168.1.107"
+ip_server = "192.168.1.100"
 
 joystickCode = threading.Thread(target=joystickServer.main, args = (ip_server,))
 #lightCode = threading.Thread(target=lightServer.main, args = ())
 #hatCode = threading.Thread(target=hatTest.main, args = (ip_server,))
 #saltWaterCode = threading.Thread(target=saltWater.main, args = ())
 inverseCode = threading.Thread(target=pilotInverseServer.main, args = (ip_server,))
-grippertopsideCode = threading.Thread(target=grippertopside.main, args = (ip_server,))
+#grippertopsideCode = threading.Thread(target=grippertopside.main, args = (ip_server,))
 #lazersCode = threading.Thread(target=lazersServer.main, args = ())
 #fryCode = threading.Thread(target=fryServer.main, args = ())
 
 
 joystickCode.start()
+#grippertopsideCode.start()
 #lightCode.start()
 #hatCode.start()
 #saltWaterCode.start()
