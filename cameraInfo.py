49db108dfa5397b8963315915905d7809abd5833
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2024-02-10 15:00:57 -0800

    d

--- cameraInfo.py
+++ cameraInfo.py
@@ -3,7 +3,7 @@ import subprocess
 
 #info = "printenv" - use for unbuntu
 info = "dir Env:"
-output = str(subprocess.check_output(info, text=True))
+output = subprocess.check_output(info, text=True)
 
 print("\n\n\n")
 print(output)
