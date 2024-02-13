bd108cd088c23cf4a20bd1674dadc19f77f44c53
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2024-02-12 22:27:22 -0800

    new

--- cameraInfo.py
+++ cameraInfo.py
@@ -1,9 +1,24 @@
 import os
 import subprocess 
+import logging
 
-#info = "printenv" - use for unbuntu
-info = "dir Env:"
+info = "printenv"
 output = subprocess.check_output(info, text=True)
 
-print("\n\n\n")
-print(output)
+logging.basicConfig(level=logging.INFO, filename = "cameraInfo.log", filemode = "w")
+
+output_line = output.splitlines() #splits data into lines?
+
+for x in range(0, len(output_line)): #goes through each linr and check is there is mention of "user"
+    if "user" in output_line[x]:
+        print(output_line[x])
+        logging.info(output_line[x])
+
+    elif "USER" in output_line[x]:
+        print(output_line[x])
+        logging.info(output_line[x])
+
+   # separate = output_line[x].split() #should split the line into each word
+   # for i in range(0, len(separate)):
+        
+
