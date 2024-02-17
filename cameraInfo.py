396bc43b45efa14a19556bdcacd5d28fb996f252
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2024-02-17 14:46:43 -0800

    esit

--- cameraInfo.py
+++ cameraInfo.py
@@ -3,6 +3,7 @@ import subprocess
 import logging
 
 info = "printenv"
+#info = "gci env:"
 output = subprocess.check_output(info, text=True)
 
 logging.basicConfig(level=logging.INFO, filename = "cameraInfo.log", filemode = "w")
@@ -10,14 +11,14 @@ logging.basicConfig(level=logging.INFO, filename = "cameraInfo.log", filemode = 
 output_line = output.splitlines() #splits data into lines?
 
 for x in range(0, len(output_line)): #goes through each linr and check is there is mention of "user"
-    if "user" in output_line[x]:
-        print(output_line[x])
+    if "USER=" in output_line[x]:
+        result = output_line[x]
+        print(result)
         logging.info(output_line[x])
 
-    elif "USER" in output_line[x]:
-        print(output_line[x])
-        logging.info(output_line[x])
-
+split_result = result.split("=")
+name = split_result[1]
+print(name)
    # separate = output_line[x].split() #should split the line into each word
    # for i in range(0, len(separate)):
         
