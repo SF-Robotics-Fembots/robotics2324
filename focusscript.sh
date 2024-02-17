055e6b6345a68398175ae8460133213f6e59f958
sienamarois <sienamarois@comcast.net>
2024-02-17 10:18:17 -0800

    committing bottomside changes

---
+++ focusscript.sh
@@ -0,0 +1,2 @@
+sudo v4l2-ctl -d /dev/video0 --set-ctrl=focus_automatic_continuous=0
+sudo v4l2-ctl -d /dev/video0 --set-ctrl=focus_absolute=4
---
+++ startup.sh
@@ -0,0 +1,4 @@
+#filepath
+
+bash focusscript.sh
+bash camerarun.sh & python3 main.py && fg
