37070dbe29eaa8ac97955a0a561eded19c03f68e
kin tirumala <100647842+ktirumala@users.noreply.github.com>
2024-03-27 15:50:18 -0700

    Update joystickServer.py
    changing joystickServer

--- joystickServer.py
+++ joystickServer.py
@@ -7,6 +7,9 @@ import time
 def main(ip_server):
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print(ip_server)
+
+    #ratio for thrust speeds
+    ratio = 0.5 #50%
     
     pygame.joystick.init()
     joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
@@ -19,6 +22,9 @@ def main(ip_server):
     
     (clientConnected, clientAddress) = serverSocket.accept()
     
+
+    slow_speed = 0
+
     while True:
         print("joystick loop")
         for event in pygame.event.get():
@@ -30,7 +36,7 @@ def main(ip_server):
                 print(event)
     
     # collect joystick values as -1 to 1
-        dockButton = (pygame.joystick.Joystick(0).get_button(4))
+        '''dockButton = (pygame.joystick.Joystick(0).get_button(4))
         if(dockButton == 1):
             num = 400
             for _ in range(num):
@@ -42,21 +48,30 @@ def main(ip_server):
                 clientConnected.send(rDir.encode())
                 vDir = "v0.0"
                 clientConnected.send(vDir.encode())
-                time.sleep(.01)
+                time.sleep(.01)'''
+        
+        if pygame.joystick.Joystick(0).get_button(6): slow_speed = 0
+        if pygame.joystick.Joystick(0).get_button(4): slow_speed = 1
+
 
         x_speed = (pygame.joystick.Joystick(0).get_axis(0))
+        if slow_speed: 
+            x_speed = x_speed*ratio
         clientConnected.send(("x" + repr(x_speed)).encode())
         print("x_speed: " + str(x_speed))
-        #time.sleep(5)
+        
         y_speed = (pygame.joystick.Joystick(0).get_axis(1))
+        if slow_speed: y_speed = y_speed*ratio
         clientConnected.send(("y" + repr(y_speed)).encode())
         print("y_speed: " + str(y_speed))
-        #time.sleep(5)
+       
         r_speed = (pygame.joystick.Joystick(0).get_axis(2))
+        if slow_speed: r_speed = r_speed*ratio
         clientConnected.send(("r" + repr(r_speed)).encode())
         print("r_speed: " + str(r_speed))
-        #time.sleep(5)
+       
         v_speed = (pygame.joystick.Joystick(0).get_axis(3))
+        if slow_speed: v_speed = v_speed*ratio
         clientConnected.send(("v" + repr(v_speed)).encode())
         print("v_speed: " + str(v_speed))
         time.sleep(.01)
