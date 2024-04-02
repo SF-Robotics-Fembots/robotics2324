4c7ef57aa480b4d1704a7cd38cd1c8d667022342
kin tirumala <25tirumala@stfrancishs.org>
2024-04-02 10:33:27 -0700

    changing ustreamer filepaths

--- camerarun.sh
+++ camerarun.sh
@@ -1,1 +1,1 @@
-/ustreamer --device=/dev/video0 --host=192.168.1.99 --format=MJPEG --port=8080 --device-timeout 3 -r 640x480 -b 1 --workers 1
+/home/geneseas/ustreamer/ustreamer --device=/dev/video0 --host=192.168.1.99 --format=MJPEG --port=8080 --device-timeout 3 -r 640x480 -b 1 --workers 1
