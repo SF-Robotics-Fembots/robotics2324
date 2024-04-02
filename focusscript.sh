ac53f45bcdd5ca98d4d8c08490863148c10ef973
kin tirumala <25tirumala@stfrancishs.org>
2024-04-02 11:06:56 -0700

    more cam edits!

--- focusscript.sh
+++ focusscript.sh
@@ -2,3 +2,7 @@ sudo v4l2-ctl -d /dev/video0 --set-ctrl=focus_automatic_continuous=0
 sudo v4l2-ctl -d /dev/video0 --set-ctrl=focus_absolute=4
 sudo v4l2-ctl -d /dev/video2 --set-ctrl=focus_automatic_continuous=0
 sudo v4l2-ctl -d /dev/video2 --set-ctrl=focus_absolute=4
+sudo v4l2-ctl -d /dev/video4 --set-ctrl=focus_automatic_continuous=0
+sudo v4l2-ctl -d /dev/video4 --set-ctrl=focus_absolute=4
+sudo v4l2-ctl -d /dev/video6 --set-ctrl=focus_automatic_continuous=0
+sudo v4l2-ctl -d /dev/video6 --set-ctrl=focus_absolute=4
