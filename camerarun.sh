68189efe4f3444b7b50cbeb450a388b200e19102
gabby rosario <25Rosario@stfrancishs.org>
2024-04-03 17:11:38 -0700

    changing dev-timeout :)

--- camerarun.sh
+++ camerarun.sh
@@ -1,1 +1,1 @@
-/home/geneseas/ustreamer/ustreamer --device=/dev/video$1 --host=192.168.1.99 --format=MJPEG --port=808$1 --device-timeout 4 -r 640x480 -b 1 --workers 1
+/home/geneseas/ustreamer/ustreamer --device=/dev/video$1 --host=192.168.1.99 --format=MJPEG --port=808$1 --device-timeout 2 -r 640x480 -b 1 --workers 1
