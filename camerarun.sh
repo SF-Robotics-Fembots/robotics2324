84b0ea551ba9f8c73a9a22d87a99b4ddd49cf97a
gabby rosario <25Rosario@stfrancishs.org>
2024-04-06 12:38:25 -0700

    cam changes :_

--- camerarun.sh
+++ camerarun.sh
@@ -1,1 +1,1 @@
-/home/geneseas/ustreamer/ustreamer --device=/dev/video$1 --host=192.168.1.99 --format=MJPEG --port=808$1 --device-timeout 2 -r 320x240 -b 1 --workers 1
+/home/geneseas/ustreamer/ustreamer --device=/dev/video$1 --host=192.168.1.99 --format=MJPEG --port=808$1 --device-timeout 2 -r 320x240 -b 2 --workers 2
