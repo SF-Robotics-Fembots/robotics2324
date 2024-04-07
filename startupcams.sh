0327b7e12e9fce2956edb5d530f8ef259a364159
gabby rosario <25Rosario@stfrancishs.org>
2024-04-06 17:53:37 -0700

    sleep 5 start cams

--- startupcams.sh
+++ startupcams.sh
@@ -3,7 +3,7 @@
 rmmod uvcvideo
 modprobe uvcvideo quirks=0x80
 bash focusscript.sh
-bash camerarun.sh 0 & sleep 1 
-bash camerarun.sh 2 & sleep 1 
-bash camerarun.sh 4 & sleep 1 
+bash camerarun.sh 0 & sleep 5 
+bash camerarun.sh 2 & sleep 5 
+bash camerarun.sh 4 & sleep 5 
 bash camerarun.sh 6
