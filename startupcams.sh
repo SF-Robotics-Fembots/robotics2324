4b46dce012e983792270d3af2f6e77eb5454b520
gabby rosario <25Rosario@stfrancishs.org>
2024-04-08 17:44:51 -0700

    back to old version i had false hope

--- startupcams.sh
+++ startupcams.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rmmod uvcvideo
-modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
+modprobe uvcvideo quirks=0x80
 bash focusscript.sh
 bash camerarun.sh 0 & sleep 5 
 bash camerarun.sh 2 & sleep 5 
