5c0a19e1e04868e761f96037bab0bc621dda654a
akrenomeron <108841178+akrenomeron@users.noreply.github.com>
2024-09-21 10:02:20 -0700

    sa

--- practiceGUI.py
+++ practiceGUI.py
@@ -15,12 +15,12 @@ import pygetwindow
 from PIL import Image
 import numpy as np
 
+video = cv2.VideoCapture(0)
 #create main window object
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         title = "photogrammetry"
-        #video = cv2.VideoCapture(0)
         # start_point = (0, 0)
         # end_point = (250, 250)
         # color = (0, 255, 0)
@@ -44,10 +44,10 @@ class MainWindow(QWidget):
         self.VBL.addWidget(self.CancelBTN)
 
         #define worker1 thread in main program
-        #self.Worker1 = Worker1()
+        self.Worker1 = Worker1()
 
-        #self.Worker1.start()
-       # self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+        self.Worker1.start()
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
         self.setLayout(self.VBL)
 
         self.ssBTN = QPushButton("Screenshot")
@@ -66,15 +66,15 @@ class MainWindow(QWidget):
         self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
 
     def CancelFeed(self):
-        #self.Worker1.stop()
+        self.Worker1.stop()
         time.sleep(0)
         cv2.destroyAllWindows()
 
     def screenshot(self):
             #titles = pygetwindow.getAllTitles() #prob dont need this
             random = int(time.time())
-            file = "C:/Users/rosar/Downloads/guiSS/" + str(random) + ".png"
-            window = pygetwindow.getWindowsWithTitle('photogrammetry')[0]
+            file = "C:/Users/alyss/Downloads/" + str(random) + ".png"
+            window = pygetwindow.getWindowsWithTitle('screenshot')[0]
             left, top = window.topleft
             right, bottom = window.bottomright
             pg.screenshot(file)
@@ -82,30 +82,92 @@ class MainWindow(QWidget):
             im = im.crop((left+19, top+42, right-19, bottom-106))
             im.save(file)
             im.show(file)
-            
+
     def screenrecord(self):
-            cam = cv2.VideoCapture(0)
-            width = int(cam.get(3))
-            height = int(cam.get(4))
+            #cam = cv2.VideoCapture(0)
+            width = int(video.get(3))
+            height = int(video.get(4))
             fps = 20
             out = cv2.VideoWriter("C:/Users/alyss/Downloads/vid.avi", cv2.VideoWriter_fourcc('M','J', 'P', 'G'), fps, (width, height))
 
-            while cam.isOpened():
-                retu, frameu = cam.read()
-                if retu == True:
-                    out.write(frameu)
-                    cv2.imshow('frameu', frameu)
+            while video.isOpened():
+                ret, frame = video.read()
+                if ret == True:
+                    out.write(frame)
+                    cv2.imshow('frame', frame)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
-                    else:
-                        break
+                else:
+                    break
             
-            cam.release()
+            #video.release()
             out.release()
 
-            cv2.destroyWindow('frameu')
+            cv2.destroyWindow('frame')
 
 #makes connection with camera and captures vid
+class Worker1(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+    #define run function
+    def run(self):
+        self.ThreadActive = True
+        #capture video
+        #video = cv2.VideoCapture(0)
+        # Read logo and resize
+        # logo = cv2.imread(r'C:/Users/rosar/Downloads/redBar.png')
+        # logo2 = cv2.imread(r'C:/Users/rosar/Downloads/redBar.png')
+        # size = 150
+        # logo = cv2.resize(logo, (size, 10), (600,600))
+        # logo2 = cv2.resize(logo2, (size, 10), (500,500))
+        # # Create a mask of logo
+        # img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+        # img3gray = cv2.cvtColor(logo2, cv2.COLOR_BGR2GRAY)
+        # ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+        # ret2, mask2 = cv2.threshold(img3gray, 1, 255, cv2.THRESH_BINARY) #probably dont need ret2
+        
+        while self.ThreadActive:
+            if cv2.waitKey(1) == ord('q'):
+                    break
+            if video.isOpened() == False:
+                 print("Failed to open video")
+            # Region of Image (ROI), where we want to insert logo
+            # roi = ((frame[200:150, 200:150]))
+            # roi2 = ((frame[200:150, 200:150]))
+            # roi = ((frame[-size-175:-175, -size-40:-40]))
+            # roi2 = ((frame[-size-175:-175, -size-450:-450]))
+            # # Set an index of where the mask is
+            # roi[np.where(mask)] = 0
+            # roi2[np.where(mask2)] = 0
+            # roi += logo
+            # roi2 += logo2
+            # print(mask)
+            # title = 'photogrammetry'
+            # start_point = (0,0)
+            # end_point = (250,250)
+            # color = (0, 255, 0)
+            # thickness = 9
+            # bar = cv2.line(video, start_point, end_point, color, thickness)
+            # cv2.imshow(title, bar)
+            # cv2.line()
+            ret, frame = video.read()
+            # width = int(video.get(1))
+            # height = int(video.get(2))
+            bar = cv2.line(frame, (280, 200), (280, 300), (0, 255, 0,), 5)
+            bar2 = cv2.line(frame, (355, 200), (355, 300), (0, 255, 0,), 5)
+            cv2.imshow('screenshot', bar)
+            cv2.imshow('screenshot', bar2)
+
+            if ret:
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImage = cv2.flip(Image, 1)
+                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                #emit thread
+                self.ImageUpdate.emit(Pic)
+
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
 
 #initialize q main window
 if __name__ == "__main__":
